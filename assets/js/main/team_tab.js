52249a4de14e64d1526790d810ec742966ad1d0e
hamke <the235style@yahoo.com>
2019-07-17 08:05:36 +0900

    Updated

--- assets/js/main/team_tab.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  $("#team_tabs").tabs();
-  $(".team_tab li a").click( function(){
-    $(".team_tab li").removeClass('on');
-    $($(this).parent()).addClass('on')
-  });
-});
--- assets/js/together/address_base.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $(document).on("click", ".searchLi", function(){
-    $(this).closest('.search_location_wrap').find('input').val($(this).text());
-  });
-  $(document).click(function(){
-    $('.searchDiv').hide();
-  });
-})
